4970e39966b2db95c77549b99c1b49c8b463832d
bradguru <35859950+bradguru2@users.noreply.github.com>
2025-01-12 22:47:34 -0800

    Update makeit.bat
    setup build

--- build/makeit.bat
+++ build/makeit.bat
@@ -2,7 +2,10 @@ echo off
 REM Build a stand-alone executable
 REM Assumes that root of project is mounted at root of c drive
 
-c:\tools\bascom c:\source\bclonejr.bas ,,, /W /O
+del bclonejr.bas
+copy c:\source\bclonejr.bas .
+c:\tools\bascom bclonejr.bas ,,, /W /O
+
 IF ERRORLEVEL 2 GOTO EXIT
 C:\tools\link bclonejr ,,,c:\tools\bascom20.lib
 
